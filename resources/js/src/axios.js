0d0322636184cc975450dfcedc8b9d201ba9d806
Darkhorse07232020 <darkhorse0723@outlook.com>
2020-09-11 17:41:31 +0800

    Finish Commit

--- resources/js/src/axios.js
+++ resources/js/src/axios.js
@@ -1,8 +1,8 @@
 // axios
 import axios from 'axios'
 
-// const baseURL = 'http://7a14824d3794.ngrok.io'
-const baseURL = 'http://localhost:8000'
+const baseURL = 'http://279d840705bb.ngrok.io'
+// const baseURL = 'http://localhost:8000'
 
 export default axios.create({
   baseURL
